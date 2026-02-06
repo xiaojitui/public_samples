b68d43bacc75b1c24b0fd8689ee895b3721a1ed8
xiaojitui <tomking576@gmail.com>
2026-02-05 21:08:31 -0600

    Update run_pipeline.py

--- call_analysis/run_pipeline.py
+++ call_analysis/run_pipeline.py
@@ -15,12 +15,39 @@ clustered_qs = cluster_questions(questions)
 
 themes = generate_themes(clustered_qs)
 
-# responses per question
-response_map = {}
-for qa in all_qa:
-    response_map.setdefault(qa["question"], []).append(qa["agent_response"])
+# # responses per question
+# response_map = {}
+# for qa in all_qa:
+#     response_map.setdefault(qa["question"], []).append(qa["agent_response"])
+# clustered_responses = {
+#     q: cluster_responses(resps)
+#     for q, resps in response_map.items()
+# }
+
+
+def build_question_mapping(clustered_questions):
+    mapping = {}
+    for group in clustered_questions:
+        canonical = group["canonical_question"]
+        for variant in group["variants"]:
+            mapping[variant] = canonical
+    return mapping
+
+from collections import defaultdict
+
+def aggregate_responses_by_canonical_question(all_qa, clustered_questions):
+    q_map = build_question_mapping(clustered_questions)
+    response_buckets = defaultdict(list)
+
+    for qa in all_qa:
+        raw_q = qa["question"]
+        canonical_q = q_map.get(raw_q)
+        if canonical_q:
+            response_buckets[canonical_q].append(qa["agent_response"])
+
+    return response_buckets
 
 clustered_responses = {
-    q: cluster_responses(resps)
-    for q, resps in response_map.items()
+    canonical_q: cluster_responses(responses)
+    for canonical_q, responses in response_buckets.items()
 }
